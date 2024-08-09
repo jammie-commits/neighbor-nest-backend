b2dff2f481869880566a5c632d907672896602e0
Gathuku Samwel <gathsam3446@gmail.com>
2024-08-09 16:23:25 +0300

    Creating Fake neighbourhoods

--- server/seed.py
+++ server/seed.py
@@ -10,3 +10,13 @@ def create_fake_data():
     with app.app_context():
         db.drop_all()
         db.create_all()
+
+# Create fake neighborhoods
+        for _ in range(5):
+            neighborhood = Neighborhood(
+                name=fake.city(),
+                description=fake.text(max_nb_chars=200)
+            )
+            db.session.add(neighborhood)
+
+        db.session.commit()
